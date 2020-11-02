a6438e76d30ad813dbbb98731d89eb3d4a96a74a
ByteAlex <bytealex@zerotwo.bot>
2020-11-02 19:13:25 +0100

    Fixed typo in prev commit

--- src/constants.rs
+++ src/constants.rs
@@ -13,7 +13,7 @@ lazy_static! {
     Authorization token provided to the endpoint in Authorization header
     */
     pub static ref VOTE_ENDPOINT_AUTH_TOKEN: String = var("VOTE_ENDPOINT_AUTH_TOKEN")
-        .unwrap_or(var("VOTE_AUTH_TOKEN").unwrap_or("secret".to_owner()));
+        .unwrap_or(var("VOTE_AUTH_TOKEN").unwrap_or("secret".to_owned()));
 
     /**
     Authorization token provided in the Authorization header
